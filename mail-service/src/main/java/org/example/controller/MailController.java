970c4e4473d166287ff2f4645a3c30dd6ff6f06c
Ivan <omest982@gmail.com>
2023-09-19 15:21:09 +0300

    Добавил микросервис mail-service который отвечает за обработку и отправку сообщений на почту. В нем создал с помощью JavaMailSender логику по отправке ссылок для подтверждения регистрации на почту с smtp порта.

---
+++ mail-service/src/main/java/org/example/controller/MailController.java
@@ -0,0 +1,23 @@
+package org.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.MailParams;
+import org.example.service.MailSenderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mail")
+@RequiredArgsConstructor
+public class MailController {
+    private final MailSenderService mailSenderService;
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendActivationMail(@RequestBody MailParams mailParams){
+        mailSenderService.send(mailParams);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ mail-service/src/main/java/org/example/service/impl/MailSenderServiceImpl.java
@@ -0,0 +1,39 @@
+package org.example.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.example.dto.MailParams;
+import org.example.service.MailSenderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailSenderServiceImpl implements MailSenderService {
+    private final JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String emailFrom;
+    @Value("${service.activation.uri}")
+    private String activationServiceUri;
+    @Override
+    public void send(MailParams mailParams) {
+        String subject = "Активация учетной записи";
+        String messageBody = getActivationMailBody(mailParams.getId());
+        String emailTo = mailParams.getEmailTo();
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(emailFrom);
+        simpleMailMessage.setTo(emailTo);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(messageBody);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+    private String getActivationMailBody(String id) {
+        String msg = String.format("Для завершения регистрации перейдите по ссылке:\n%s",
+                activationServiceUri);
+        return msg.replace("{id}", id);
+    }
+}
